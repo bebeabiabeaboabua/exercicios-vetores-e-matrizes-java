765468ec7e5ed7a7bdafb6a3ec297501ce33901f
Bea <beatrizheimann@gmail.com>
2025-06-30 08:06:47 -0300

    Update Ex2_Matriz.java

--- matrizes/Ex2_Matriz.java
+++ matrizes/Ex2_Matriz.java
@@ -14,19 +14,17 @@ public class Ex2_Matriz {
         int[] somaColunas = new int[5]; 
         int[] somaLinhas = new int[5]; 
 
-        // Lê e armazena os valores na matriz
         System.out.println("Digite os valores da matriz 3x5:");
 
-        for (int i = 0; i < 3; i++) { // linhas
-            for (int j = 0; j < 5; j++) { // colunas
+        for (int i = 0; i < 3; i++) { 
+            for (int j = 0; j < 5; j++) { 
                 System.out.print("Valor na posição [" + i + "][" + j + "]: ");
                 matriz[i][j] = scanner.nextInt();
 
-                // Armazena no vetor auxiliar
+
                 todosOsValores[index] = matriz[i][j];
                 index++;
 
-                // Conta pares e ímpares
                 if (matriz[i][j] % 2 == 0) {
                     pares++;
                 } else {
@@ -36,7 +34,7 @@ public class Ex2_Matriz {
             }
         }
 
- // Verifica se tem repetidos
+
         boolean temRepetido = false;
         for (int i = 0; i < todosOsValores.length; i++) {
             for (int j = i + 1; j < todosOsValores.length; j++) {
@@ -48,7 +46,7 @@ public class Ex2_Matriz {
             if (temRepetido) break;
         }
 
-        // Resultados
+
         System.out.println("\nQuantidade de números pares: " + pares);
         System.out.println("Quantidade de números ímpares: " + impares);
         System.out.println("A matriz possui elementos repetidos? " + (temRepetido ? "Sim" : "Não"));
